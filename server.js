20d9f7ae08cb25cdbb7b0d15ef226a4b4b47486d
Jerome Joof <jeromejoof@yahoo.com>
2019-12-11 10:35:34 -0800

    working on map api

--- server.js
+++ server.js
@@ -3,6 +3,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const superagent = require('superagent');
+
+
 const PORT = process.env.PORT || 3000;
 
 // Load ENV
@@ -20,8 +23,11 @@ app.get('/weather', getWeather);
 
 //Handlers
 function getLocation(request, response){
-  const locationData = searchLatToLng(request.query.data || 'Lynnwood, WA, USA');
-  response.send(locationData);
+  return searchLatToLng(request.query.data)
+    .then(locationData => {
+      response.send(locationData);
+
+    })
 }
 
 function getWeather(request, response){
@@ -37,6 +43,11 @@ function Location(location){
   this.formatted_query = location.formatted_address;
   this.latitude = location.geometry.location.lat;
   this.longitude = location.geometry.location.lng;
+  this.long_name = location.address_components[0].long_name;
+  console.log('what?', this.formatted_query)
+
+  console.log('what?', this.long_name)
+
   // weatherLocations.push(this.latitude);
   // weatherLocations.push(this.longitude);
 }
@@ -48,17 +59,24 @@ function Daily(dailyForecast){
 }
 
 //Search for data
-function searchLatToLng(){
-  const geoData = require('./data/geo.json');
-  const location = new Location(geoData.results[0]);
-  return location;
+function searchLatToLng(query){
+  const geoDataUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`
+  return superagent.get(geoDataUrl)
+    .then(geoData => {
+
+      const location = new Location(geoData.body.results[0]);
+      console.log('what', location)
+      return location;
+    })
+    .catch(err => console.error(err))
+  // const geoData = require('./data/geo.json');
 }
 
 function searchWeather(){{
   let darkSkyData = require('./data/darksky.json');
   console.log(darkSkyData);
   let weatheArray = [];
-  darkSkyData.daily.data.forEach(forecast => weatheArray.push(new Daily(forecast)));
+  darkSkyData.daily.data.map(forecast => weatheArray.push(new Daily(forecast)));
   console.log(weatheArray);
   return weatheArray;
 }}
